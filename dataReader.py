5b994319837cdf261cb6fd8f15def9b11f2362fd
= <=>
2022-02-20 20:44:16 +0000

    abandoned user interface for time being to focus on CSV output. Added functionality that creates CSV files for each patient and every patient event that occurs

--- dataReader.py
+++ dataReader.py
@@ -1,7 +1,11 @@
 import json
+import os
 from os import listdir
 from os.path import isfile, join
 import mysql.connector
+from tkinter import *
+import csv
+import pandas as pd
 
 '''
 An external system / supplier is sending patient data to our platform using the FHIR standard. 
@@ -117,72 +121,69 @@ def add_patient(patient_data, patient_id):
 
     con = connect_patient_db()
     db_cursor = con.cursor()
-    db_cursor.execute("SELECT * FROM patient WHERE unique_id='" + patient_id + "'")
-    patient = db_cursor.fetchall()
 
-    # if the patient does not exist
-    if len(patient) == 0:
-        birth_date = patient_data["birthDate"]
-        birth_sex = patient_data["extension"][3]["valueCode"]
-        mothers_maiden_name = str(patient_data["extension"][2]["valueString"]).replace("'", "''")
-        marital_status = patient_data["maritalStatus"]["text"]
-        gender = patient_data["gender"]
-        family_name = str(patient_data["name"][0]["family"]).replace("'", "''")
-        name_use = patient_data["name"][0]["use"]
-        disability_adjusted_life_years = str(patient_data["extension"][5]["valueDecimal"])
-        quality_adjusted_life_years = str(patient_data["extension"][6]["valueDecimal"])
+    birth_date = patient_data["birthDate"]
+    birth_sex = patient_data["extension"][3]["valueCode"]
+    mothers_maiden_name = str(patient_data["extension"][2]["valueString"]).replace("'", "''")
+    marital_status = patient_data["maritalStatus"]["text"]
+    gender = patient_data["gender"]
+    family_name = str(patient_data["name"][0]["family"]).replace("'", "''")
+    name_use = patient_data["name"][0]["use"]
+    disability_adjusted_life_years = str(patient_data["extension"][5]["valueDecimal"])
+    quality_adjusted_life_years = str(patient_data["extension"][6]["valueDecimal"])
 
-        home_address_line = str(patient_data["address"][0]["line"][0]).replace("'", "''")
-        home_address_city = str(patient_data["address"][0]["city"]).replace("'", "''")
-        home_address_state = str(patient_data["address"][0]["state"]).replace("'", "''")
-        home_address_country = str(patient_data["address"][0]["country"]).replace("'", "''")
-        home_address_longitude = str(patient_data["address"][0]["extension"][0]["extension"][0]["valueDecimal"])
-        home_address_latitude = str(patient_data["address"][0]["extension"][0]["extension"][1]["valueDecimal"])
+    home_address_line = str(patient_data["address"][0]["line"][0]).replace("'", "''")
+    home_address_city = str(patient_data["address"][0]["city"]).replace("'", "''")
+    home_address_state = str(patient_data["address"][0]["state"]).replace("'", "''")
+    home_address_country = str(patient_data["address"][0]["country"]).replace("'", "''")
+    home_address_longitude = str(patient_data["address"][0]["extension"][0]["extension"][0]["valueDecimal"])
+    home_address_latitude = str(patient_data["address"][0]["extension"][0]["extension"][1]["valueDecimal"])
 
-        birth_place_city = str(patient_data["extension"][4]["valueAddress"]["city"]).replace("'", "''")
-        birth_place_state = str(patient_data["extension"][4]["valueAddress"]["state"]).replace("'", "''")
-        birth_place_country = str(patient_data["extension"][4]["valueAddress"]["country"]).replace("'", "''")
+    birth_place_city = str(patient_data["extension"][4]["valueAddress"]["city"]).replace("'", "''")
+    birth_place_state = str(patient_data["extension"][4]["valueAddress"]["state"]).replace("'", "''")
+    birth_place_country = str(patient_data["extension"][4]["valueAddress"]["country"]).replace("'", "''")
 
-        us_core_ethnicity = patient_data["extension"][1]["extension"][0]["valueCoding"]["display"]
-        us_core_race = patient_data["extension"][0]["extension"][0]["valueCoding"]["display"]
+    us_core_ethnicity = patient_data["extension"][1]["extension"][0]["valueCoding"]["display"]
+    us_core_race = patient_data["extension"][0]["extension"][0]["valueCoding"]["display"]
 
-        try:
-            prefix = patient_data["name"][0]["prefix"][0]
-        except:
-            prefix = ""
+    try:
+        prefix = patient_data["name"][0]["prefix"][0]
+    except:
+        prefix = ""
 
-        try:
-            death_date_time = patient_data["deceasedDateTime"]
-        except:
-            death_date_time = "n/a"
+    try:
+        death_date_time = patient_data["deceasedDateTime"]
+    except:
+        death_date_time = "n/a"
 
-        try:
-            multiple_birth = str(patient_data["multipleBirthBoolean"])
-        except:
-            multiple_birth = ""
+    try:
+        multiple_birth = str(patient_data["multipleBirthBoolean"])
+    except:
+        multiple_birth = ""
 
-        given_name = ""
-        for name in patient_data["name"][0]["given"]:
-            given_name += name + " "
-        given_name = given_name[:-1].replace("'", "\'")
+    given_name = ""
+    for name in patient_data["name"][0]["given"]:
+        given_name += name + " "
+    given_name = given_name[:-1].replace("'", "\'")
 
-        try:
-            sql = "INSERT INTO patient_database.patient (`patient_id`,`unique_id`,`given_name`,`family_name`," \
-                  "`birth_date`,`birth_sex`,`gender`,`mother`,`marital_status`,`name_use`,`address_line`," \
-                  "`address_city`,`address_state`,`address_country`,`address_latitude`,`address_longitude`," \
-                  "`birth_city`,`birth_state`,`birth_country`,`us_core_ethnicity`,`us_core_race`,`prefix`," \
-                  "`death_dateTime`,`multiple_birth`,`disability_adjusted_life_years`,`quality_adjusted_life_years`) " \
-                  "VALUES (NULL, '" + patient_id + "', '" + given_name + "', '" + family_name + "', '" + birth_date +\
-                  "', '" + birth_sex + "', '" + gender + "', '" + mothers_maiden_name + "', '" + marital_status + "', '" + name_use + "', '" + home_address_line + "', '" + home_address_city + "', '" + home_address_state + "','" + home_address_country + "', '" + home_address_latitude + "', '" + home_address_longitude + "', '" + birth_place_city + "', '" + birth_place_state + "', '" + birth_place_country + "', '" + us_core_ethnicity + "', '" + us_core_race + "', '" + prefix + "', '" + death_date_time + "', '" + multiple_birth + "', '" + disability_adjusted_life_years + "', '" + quality_adjusted_life_years + "') "
-            db_cursor.execute(sql.rstrip("\n"))
-            con.commit()
-        except Exception as e:
-            print()
-            print("ERROR: Could not add "+given_name+" "+family_name)
-            print()
-            con.close()
-            return None
-    db_patient_id = db_cursor.lastrowid
+    try:
+        sql = "INSERT INTO patient_database.patient (`patient_id`,`unique_id`,`given_name`,`family_name`," \
+              "`birth_date`,`birth_sex`,`gender`,`mother`,`marital_status`,`name_use`,`address_line`," \
+              "`address_city`,`address_state`,`address_country`,`address_latitude`,`address_longitude`," \
+              "`birth_city`,`birth_state`,`birth_country`,`us_core_ethnicity`,`us_core_race`,`prefix`," \
+              "`death_dateTime`,`multiple_birth`,`disability_adjusted_life_years`,`quality_adjusted_life_years`) " \
+              "VALUES (NULL, '" + patient_id + "', '" + given_name + "', '" + family_name + "', '" + birth_date + \
+              "', '" + birth_sex + "', '" + gender + "', '" + mothers_maiden_name + "', '" + marital_status + "', '" + name_use + "', '" + home_address_line + "', '" + home_address_city + "', '" + home_address_state + "','" + home_address_country + "', '" + home_address_latitude + "', '" + home_address_longitude + "', '" + birth_place_city + "', '" + birth_place_state + "', '" + birth_place_country + "', '" + us_core_ethnicity + "', '" + us_core_race + "', '" + prefix + "', '" + death_date_time + "', '" + multiple_birth + "', '" + disability_adjusted_life_years + "', '" + quality_adjusted_life_years + "') "
+        db_cursor.execute(sql.rstrip("\n"))
+        con.commit()
+        db_patient_id = db_cursor.lastrowid
+    except Exception as e:
+        print()
+        print("ERROR: Could not add " + given_name + " " + family_name)
+        print()
+        con.close()
+        return None
+
     con.close()
     return db_patient_id
 
@@ -199,7 +200,8 @@ def add_patient_language(language, patient_id):
     con = connect_patient_db()
     db_cursor = con.cursor()
     try:
-        sql = "INSERT INTO `patient_database`.`patient_language`(`patient_language_id`,`patient`,`language`) VALUES (NULL,'"+str(patient_id)+"','"+language+"');"
+        sql = "INSERT INTO `patient_database`.`patient_language`(`patient_language_id`,`patient`,`language`) VALUES (NULL,'" + str(
+            patient_id) + "','" + language + "');"
         db_cursor.execute(sql.rstrip("\n"))
         con.commit()
     except Exception as e:
@@ -218,7 +220,7 @@ def add_patient_identifier(identifier, patient_id):
     :return:
     """
     try:
-        identifier_type = str(identifier["type"]["text"]).replace("'","''")
+        identifier_type = str(identifier["type"]["text"]).replace("'", "''")
     except:
         identifier_type = "-"
     identifier_system = identifier["system"]
@@ -226,7 +228,8 @@ def add_patient_identifier(identifier, patient_id):
     con = connect_patient_db()
     db_cursor = con.cursor()
     try:
-        sql = "INSERT INTO `patient_database`.`patient_identifier`(`patient_identifier_id`,`patient`,`id_system`,`type`,`value`)VALUES (NULL, '"+str(patient_id)+"','"+identifier_system+"', '"+identifier_type+"', '"+identifier_value+"');"
+        sql = "INSERT INTO `patient_database`.`patient_identifier`(`patient_identifier_id`,`patient`,`id_system`,`type`,`value`)VALUES (NULL, '" + str(
+            patient_id) + "','" + identifier_system + "', '" + identifier_type + "', '" + identifier_value + "');"
         db_cursor.execute(sql.rstrip("\n"))
         con.commit()
     except Exception as e:
@@ -279,7 +282,8 @@ def add_event(event_data, patient_id):
     db_cursor = con.cursor()
     try:
         formatted_event_data = json.dumps(event_data).replace('"', '""').replace("'", "''")
-        sql = "INSERT INTO `patient_database`.`patient_event`(`patient_event_id`,`patient`,`event_data`, `type`)VALUES (NULL, '" + str(patient_id) + "','" + formatted_event_data + "','" + event_data["resource"]["resourceType"] + "');"
+        sql = "INSERT INTO `patient_database`.`patient_event`(`patient_event_id`,`patient`,`event_data`, `type`)VALUES (NULL, '" + str(
+            patient_id) + "','" + formatted_event_data + "','" + event_data["resource"]["resourceType"] + "');"
         db_cursor.execute(sql.rstrip("\n"))
         con.commit()
     except Exception as e:
@@ -301,26 +305,32 @@ def process_json(json_data, file_path):
         patient_data = json_data["entry"][0]["resource"]
         unique_id = patient_data["id"]
 
-        # Add the patient to the database
-        db_patient_id = add_patient(patient_data, unique_id)
+        con = connect_patient_db()
+        db_cursor = con.cursor()
+        db_cursor.execute("SELECT * FROM patient WHERE unique_id='" + unique_id + "'")
+        patient = db_cursor.fetchall()
 
-        # record patient languages in the database
-        for language in patient_data["communication"]:
-            add_patient_language(language["language"]["text"], db_patient_id)
+        # if the patient does not exist
+        if len(patient) == 0:
+            # Add the patient to the database
+            db_patient_id = add_patient(patient_data, unique_id)
 
-        # record patient contact information in the database
-        for telecom in patient_data["telecom"]:
-            add_patient_contact(telecom, db_patient_id)
+            # record patient languages in the database
+            for language in patient_data["communication"]:
+                add_patient_language(language["language"]["text"], db_patient_id)
 
-        # record patient identifiers in the database
-        for identifier in patient_data["identifier"]:
-            add_patient_identifier(identifier, db_patient_id)
+            # record patient contact information in the database
+            for telecom in patient_data["telecom"]:
+                add_patient_contact(telecom, db_patient_id)
 
-        # record all patient events in the database
-        for event in json_data["entry"]:
-            if event["resource"]["resourceType"] != "Patient":
-                add_event(event, db_patient_id)
+            # record patient identifiers in the database
+            for identifier in patient_data["identifier"]:
+                add_patient_identifier(identifier, db_patient_id)
 
+            # record all patient events in the database
+            for event in json_data["entry"]:
+                if event["resource"]["resourceType"] != "Patient":
+                    add_event(event, db_patient_id)
     else:
         print("ERROR: Patient Details not found for " + file_path)
 
@@ -368,10 +378,124 @@ def load_json_files(directory):
     return True
 
 
+def create_csv_files():
+    # Check is csv folder exists if not create it
+    if not os.path.isdir('csv'):
+        os.mkdir('csv')
+
+    # for each patient create a directory and fill it with events and patient csv files
+    con = connect_patient_db()
+    db_cursor = con.cursor()
+    db_cursor.execute("SELECT * FROM patient")
+    patients = db_cursor.fetchall()
+
+    db_cursor.execute("SHOW COLUMNS FROM patient")
+    headers = db_cursor.fetchall()
+    header = []
+    for item in headers:
+        header.append(item[0])
+
+    for patient in patients:
+        folder_name = patient[2] + "_" + patient[3] + "_" + patient[1]
+        if not os.path.isdir('csv/' + folder_name):
+            os.mkdir('csv/' + folder_name)
+
+        # Create patient file
+        with open('csv/' + folder_name + '/patient.csv', 'w', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+
+            # write the header
+            writer.writerow(header)
+
+            # write multiple rows
+            writer.writerow(patient)
+
+        # Create every event file
+        db_cursor.execute("SELECT * FROM patient_event WHERE patient=" + str(patient[0]))
+        events = db_cursor.fetchall()
+
+        for event in events:
+            # Create event file
+            df = pd.read_json(event[2])
+            df.to_csv('csv/' + folder_name + '/event_' + event[0] + '_' + event[3] + '.csv', index=None)
+
+    con.close()
+
+    return True
+
+
+def load_app_interface():
+    app = Tk()
+    app.title("Medical Records")
+    app.geometry("900x600")
+    app.resizable(width=0, height=0)
+    app['bg'] = 'white'
+
+    frame = Frame(app)
+    frame.pack(expand=True, fill=BOTH)  # .grid(row=0,column=0)
+    canvas = Canvas(frame, bg='#FFFFFF', width=900, height=600)
+    hbar = Scrollbar(frame, orient=HORIZONTAL)
+    hbar.pack(side=BOTTOM, fill=X)
+    hbar.config(command=canvas.xview)
+    vbar = Scrollbar(frame, orient=VERTICAL)
+    vbar.pack(side=RIGHT, fill=Y)
+    vbar.config(command=canvas.yview)
+    canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+
+    con = connect_patient_db()
+    db_cursor = con.cursor()
+    db_cursor.execute("SELECT given_name, family_name, unique_id FROM patient")
+    patients = db_cursor.fetchall()
+    con.close()
+
+    # this wil create a label widget
+    l1 = Label(canvas, text="Unique ID", width=37)
+    l2 = Label(canvas, text="Given Name", width=18)
+    l3 = Label(canvas, text="Family Name", width=18)
+    l4 = Label(canvas, text="Events", width=18)
+    l5 = Label(canvas, text="Profile CSV", width=20)
+    l6 = Label(canvas, text="Events CSV", width=10)
+
+    # grid method to arrange labels in respective
+    # rows and columns as specified
+    l1.grid(row=0, column=1, sticky=W, pady=2)
+    l2.grid(row=0, column=2, sticky=W, pady=2)
+    l3.grid(row=0, column=3, sticky=W, pady=2)
+    l4.grid(row=0, column=4, sticky=W, pady=2)
+    l5.grid(row=0, column=5, sticky=W, pady=2)
+    l6.grid(row=0, column=6, sticky=W, pady=2)
+
+    i = 1
+    for patient in patients:
+        l1 = Label(canvas, text=patient[2], width=37)
+        l2 = Label(canvas, text=patient[0], width=18)
+        l3 = Label(canvas, text=patient[1], width=18)
+        l4 = Label(canvas, text="Events", width=18)
+        l5 = Label(canvas, text="Profile CSV", width=20)
+        l6 = Label(canvas, text="Events CSV", width=10)
+
+        l1.grid(row=i, column=1, sticky=W, pady=1)
+        l2.grid(row=i, column=2, sticky=W, pady=1)
+        l3.grid(row=i, column=3, sticky=W, pady=1)
+        l4.grid(row=i, column=4, sticky=W, pady=1)
+        l5.grid(row=i, column=5, sticky=W, pady=1)
+        l6.grid(row=i, column=6, sticky=W, pady=1)
+        i += 1
+
+    canvas.configure(scrollregion=canvas.bbox("all"))
+    canvas.pack(side=LEFT, expand=True, fill=BOTH)
+
+    app.mainloop()
+
+
 if __name__ == '__main__':
     # check if database exists or create if it doesn't exist
     if init_database():
         # load data files and process to fill database
         load_json_files("data")
 
+        # create csv files
+        create_csv_files()
+
         # load interface
+        # load_app_interface()
